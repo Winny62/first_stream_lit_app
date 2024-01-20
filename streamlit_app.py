fa4d8c7ede2cca362b4d343375b8a6c86629400e
Winny62 <157219161+Winny62@users.noreply.github.com>
2024-01-20 15:22:49 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Strawberry & Banana Smothie')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothis 🥝🍇')
+
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list ("pick some fruits:", list(my_fruit_list.index))
