d84c59eb2fcba7b7541f0eac946d2af0bde293e8
Winny62 <157219161+Winny62@users.noreply.github.com>
2024-01-20 14:50:47 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Strawberry & Banana Smothie')
 
+streamlit.header('Build Your Own Fruit Smoothis')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
