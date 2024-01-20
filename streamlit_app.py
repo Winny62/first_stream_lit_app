fe649355febfbe90d6a38979fec0409262f58baf
Winny62 <157219161+Winny62@users.noreply.github.com>
2024-01-20 14:40:30 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('Omega 3 Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Strawberry & Banana Smothie')
+
+import pandas
